d4f87bef6ea82bd5d823509bf9c229b51705094c
totokc <totokc@yahoo.com>
2018-05-03 20:34:00 +0700

    latihan variable di js

---
+++ variable.js
@@ -0,0 +1,40 @@
+// old version
+// var name = 'toto'
+
+// new version
+// const city = 'Jakarta'
+// let phone = '0812345678'
+// console.log(phone)
+
+// keyword const vs let
+
+const angka_satu = 8;
+const angka_dua = 2;
+let hasil;
+
+const hasil_tandingan = angka_satu + angka_dua;
+hasil = hasil_tandingan;
+console.log(hasil);
+
+// 2. Data type
+// const nama = "yogie"; // string
+// const umur = 17; // integer
+// const nominal = 20.5; // double
+// const isHandsome = true; // boolean - true / false
+// const cars = ["honda", "toyota", "mitsubishi"]; // array
+// const angkas = [1, 2, 3, 4]; // array of number
+// const info = {
+// address: "Jl. Semangka",
+// city: "Jakarta",
+// zip_code: "65123",
+// ishandsome; true,
+// angka: [1, 2, 3, 4]
+// };   // Object
+
+console.log(nama)
+console.log(nama + ' umur saya: ' + umur)
+console.log(cars[1])  //  array always has index
+console.log(info.city)
+
+2018
+
